6338724ea2794a22dfc0d36e517c4ccb577bd615
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-06-13 09:42:42 +0200

    tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,21 @@
 function formarPalabras(inputs){
-  return null;
+  if(inputs === null){
+    return "null"
+  }else if(inputs.length === 0){
+    return "vacio"
+  }else{
+    let filtering = inputs.filter((e, i) => {
+      if(i % 2 === 0){
+        return e
+      }
+    })
+    if((inputs.length - 1)%2 !== 0){
+      let last = inputs.slice(-1)
+      return filtering.concat(last).join('')
+    }else{
+      return filtering.join('')
+    }
+  }
 }
 
 module.exports = formarPalabras;
